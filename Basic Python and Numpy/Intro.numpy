e56cf3834a39e53f04b75deb1407ae33ac594f54
Kuldeep2047 <kuldeepgarg6993@gmail.com>
2025-06-23 15:13:57 +0530

    Initial commit - Emotion Detection project

--- Basic Python and Numpy/Intro.numpy
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-n1 = np.arange(1,101,4)
-print(n1)
--- Basic Python and Numpy/file.py
+++
@@ -1,7 +0,0 @@
-f1 = open("question.txt", "r+")
-l = ["s1","s2","s3"]
-for i in l:
-    f1.write(i+'\n')
-    
-for i in f1.readline:
-    print(i,end='0')
